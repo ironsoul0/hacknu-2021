42c2fedbd55f99166d571c71ae5e3f45f4f37400
Rauan Omarov <rauan.omarov124@gmail.com>
2021-03-21 07:45:24 +0600

    Changed dashboard

--- src/components/GameCard/component.tsx
+++ src/components/GameCard/component.tsx
@@ -2,41 +2,62 @@ import React, { FC, forwardRef } from 'react';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 
-import { PlayIcon, ChartIcon } from '../../core';
+import { PlayIcon, ChartIcon, ChimpIcon, ReactionIcon, NumberMemoryIcon, GameType } from '../../core';
 import { Props } from './props';
 
 export const GameCard: FC<Props> = forwardRef<HTMLDivElement, Props>(
   ({ name, id, percentile, points, unit, className }: Props, ref) => {
+    const gameIcons = {
+      reactionTime: <ReactionIcon className="w-44" />,
+      numberMemory: <NumberMemoryIcon className="w-44" />,
+      chimpTest: <ChimpIcon className="w-44" />,
+    };
+
     return (
-      <div className={clsx('flex justify-between items-center gap-x-4', className)} ref={ref}>
-        <div className="flex-1">
-          <p className="font-bold text-md mb-3">{name}</p>
-          <Link to={`/game/${id}`} className="text-blue-500 font-medium flex items-center">
-            <PlayIcon className="w-5" />
-            <p className="ml-1.5">Играть</p>
-          </Link>
-          <Link to={`/leaderboard/${id}`} className="text-blue-500 font-medium flex items-center">
-            <ChartIcon className="w-5" />
-            <p className="ml-1.5">Статистика</p>
-          </Link>
-        </div>
-        <div className="flex-1">
-          <div className="flex">
-            <p className="font-bold text-4xl">{points?.toFixed(0) || '?'}</p>
-            <p className="self-end text-lg ml-0.5">{points ? unit : ''}</p>
-          </div>
-          <div className="bg-gray-300 h-8 w-100 relative flex items-center">
-            <div className="absolute bg-blue-500 h-8 flex items-center" style={{ width: `${percentile}%` }}>
-              {percentile !== null && percentile >= 50.0 && (
-                <p className="text-white font-bold ml-auto pr-3">{percentile?.toFixed(1)}%</p>
-              )}
+      <div className={clsx('flex flex-col justify-between gap-x-4', className)} ref={ref}>
+        <div className="grid grid-cols-12">
+          <div className="col-span-6 flex justify-center">{gameIcons[id as GameType]}</div>
+          <div className="col-span-6 flex flex-col justify-center mx-5">
+            <div className="flex flex-col justify-center">
+              <p className="font-black text-lg whitespace-nowrap">{name}</p>
             </div>
-            {percentile !== null && percentile < 50.0 && (
-              <p className="text-gray-500 font-bold ml-auto pl-3" style={{ marginLeft: `${percentile}%` }}>
-                {percentile?.toFixed(1)}%
-              </p>
-            )}
-            {percentile === null && <p className="text-gray-500 font-bold ml-2">?</p>}
+            <div className="flex flex-col">
+              <div className="flex">
+                <p className="font-bold text-4xl">{points?.toFixed(0) || '?'}</p>
+                <p className="self-end text-lg ml-0.5">{points ? unit : ''}</p>
+              </div>
+              <div className="bg-gray-300 h-8 w-100 relative flex items-center">
+                <div className="absolute bg-blue-500 h-8 flex items-center" style={{ width: `${percentile}%` }}>
+                  {percentile !== null && percentile >= 50.0 && (
+                    <p className="text-white font-bold ml-auto pr-3">{percentile?.toFixed(1)}%</p>
+                  )}
+                </div>
+                {percentile !== null && percentile < 50.0 && (
+                  <p className="text-gray-500 font-bold ml-auto pl-3" style={{ marginLeft: `${percentile}%` }}>
+                    {percentile?.toFixed(1)}%
+                  </p>
+                )}
+                {percentile === null && <p className="text-gray-500 font-bold ml-2">?</p>}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-12">
+          <div className="col-span-6 flex justify-center">
+            <div className="container bg-gray-100 rounded-full flex justify-center items-center mx-3 p-3 shadow-sm">
+              <Link to={`/game/${id}`} className="text-blue-500 text-xl font-medium flex items-center">
+                <PlayIcon className="w-10" />
+                <p className="ml-1.5">Играть</p>
+              </Link>
+            </div>
+          </div>
+          <div className="col-span-6 flex justify-center">
+            <div className="container bg-gray-100 rounded-full flex justify-center items-center mx-3 p-3 shadow-sm">
+              <Link to={`/leaderboard/${id}`} className="text-blue-500 text-xl font-medium flex items-center">
+                <ChartIcon className="w-7" />
+                <p className="ml-1.5">Друзья</p>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
