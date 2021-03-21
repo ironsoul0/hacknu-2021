44442f4493fe0689adab87d3d43635eb0b67d9ea
ironsoul0 <timka2609@gmail.com>
2021-03-21 09:11:40 +0600

    fix: display all games on animation end

--- src/components/Dashboard/component.tsx
+++ src/components/Dashboard/component.tsx
@@ -21,11 +21,8 @@ export const DashboardPage: React.FC = () => {
   return (
     <div className="px-4 pt-5">
       {me && games ? (
-        <>
+        <div>
           <div className="flex flex-col animate-smooth-appear">
-            <div className="mx-auto mb-2">
-              <p className="text-md text-gray-500">Игрок</p>
-            </div>
             <div
               className="bg-gray-300 rounded-full h-20 w-20 mx-auto mb-2"
               style={{
@@ -34,15 +31,15 @@ export const DashboardPage: React.FC = () => {
               }}
             />
             <div className="mx-auto">
-              <p className="font-bold text-3xl mb-10">{me.name + ' ' + me.lastname}</p>
+              <p className="font-bold text-3xl mb-10 text-center">{me.name + ' ' + me.lastname}</p>
             </div>
           </div>
           <div className="animate-smooth-appear">
-            {games.map((game, i) => (
-              <GameCard key={game.id} {...game} className="mb-12" ref={(ref) => (cardsRef.current[i] = ref)} />
+            {games.map((game) => (
+              <GameCard key={game.id} {...game} className="mb-12" />
             ))}
           </div>
-        </>
+        </div>
       ) : (
         <Spinner />
       )}
